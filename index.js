9ddba8af699789a71f1a34c26011a8047bcbf4f9
alijamal5750 <118554626+alijamal5750@users.noreply.github.com>
2023-02-22 18:31:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const mountRoute=require('./routes');
 const cors=require('cors');
 const compression=require('compression');
 const {webhookcheckout}=require('.//services/orderService');
+const rateLimit=require('express-rate-limit');
 const hpp=require('hpp');
 const mongoSanitize=require('express-mongo-sanitize');
 const xss=require('xss');
@@ -49,9 +50,15 @@ if (process.env.NODE_ENV === "development") {
   console.log(`mode : ${process.env.NODE_ENV}`);
 }
 
+// avoid atackers to send many requests , in the postman you will see x-Rate-limit,Remaining,Reset(time windowsMs):
+const limiter=rateLimit({
+windowMs:15*10*1000,
+max:5,
+message:'Too many requests from this ip,please try again after 15 minutes!',
+});
 
 // apply to al routes in application : 
-app.use('/api');
+app.use('/api',limiter);
 
 // prevent to send multiple parameters with the same name(select last one only) not accespt an araay :
 // not use this to array parameter : in filtering  or anything so add to whitelist (not apply this middleware):
