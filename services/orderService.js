f8d757971f200584e328de8293852732f40b5d69
alijamal5750 <118554626+alijamal5750@users.noreply.github.com>
2023-02-22 16:47:09 +0300

    Update orderService.js

--- services/orderService.js
+++ services/orderService.js
@@ -118,17 +118,13 @@ exports.checkoutSession=asyncHandler(async(req,res,next)=>{
     
     // create stripe checkout session : 
 const session=await stripe.checkout.sessions.create({
-    line_items:[ 
-        {
-        price_data:{
-        amount:totalOrderPrice * 100,
-        currency:'usd',
-        product_data:{
-        name:req.user.name,
-        },
-    },
-    quantity:1,
-    }
+    line_items: [
+      {
+        name: req.user.name,
+        amount: totalOrderPrice * 100,
+        currency: 'usd',
+        quantity: 1,
+      },
     ],
 mode:'payment',
 success_url: `${req.protocol}://${req.get('host')}/order`,
